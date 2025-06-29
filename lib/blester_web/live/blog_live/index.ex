c9b187bd29e63ed1641126386fd753e7406a668b
Zaali <zaali@live.com>
2025-06-29 14:46:25 +0400

    fix: add missing total_pages assign for blog index pagination
    - Calculate total_pages in load_posts function using ceil(total_count / per_page)
    - Initialize total_pages, total_count, and current_page in mount function
    - Fix KeyError for :total_pages not found in blog index template
    - Ensure proper pagination functionality in blog index page

--- lib/blester_web/live/blog_live/index.ex
+++ lib/blester_web/live/blog_live/index.ex
@@ -14,7 +14,7 @@ defmodule BlesterWeb.BlogLive.Index do
         _ -> []
       end
 
-    {:ok, assign(socket, posts: posts, errors: %{}, current_user_id: user_id, cart_count: cart_count)}
+    {:ok, assign(socket, posts: posts, errors: %{}, current_user_id: user_id, cart_count: cart_count, total_pages: 0, total_count: 0, current_page: 1)}
   end
 
   @impl true
@@ -45,12 +45,14 @@ defmodule BlesterWeb.BlogLive.Index do
   end
 
   defp load_posts(socket, page) do
-    offset = (page - 1) * 10
-    case Accounts.list_posts_paginated(10, offset) do
+    per_page = 10
+    offset = (page - 1) * per_page
+    case Accounts.list_posts_paginated(per_page, offset) do
       {:ok, {posts, total_count}} ->
-        assign(socket, posts: posts, total_count: total_count, current_page: page)
+        total_pages = ceil(total_count / per_page)
+        assign(socket, posts: posts, total_count: total_count, current_page: page, total_pages: total_pages)
       {:error, _} ->
-        assign(socket, posts: [], total_count: 0, current_page: page)
+        assign(socket, posts: [], total_count: 0, current_page: page, total_pages: 0)
     end
   end
 
